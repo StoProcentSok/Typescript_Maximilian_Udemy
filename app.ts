8040d921f6279018f1069c98619a29a2b2025554
Michal_Piec <m.buczek@mat.net.pl>
2018-08-07 21:06:19 +0200

    Begining of functions section.

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 console.log("It works! Hope so..");
 let myName = 'Michal';
 
-
 let myAge = 10;
 
-let hasHobbies = true;
-//hasHobbies = 0;
+var hasHobbies = true;
+
 console.log(hasHobbies);
   
