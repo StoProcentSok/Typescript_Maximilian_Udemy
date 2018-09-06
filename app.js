d0757471319c5c2823f84ef41d7ccbe01938917f
MAT\m.buczek <m.buczek@mat.net.pl>
2018-09-06 15:46:33 +0200

    Understanding Created JavaScript code

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 console.log("It works! Hope so..");
 var myName = 'Michal';
 var myAge = 35;
-var hasHobbies = true;
-hasHobbies = "false";
-console.log(hasHobbies);
+//string array
+var hobbies = ["silownia", "modele"];
+console.log(typeof (hobbies));
+//number array, 
+var numberArray = [100];
+console.log(typeof (numberArray));
+//tuple
+var tupleTest = ["tekstowa", 100];
+//enum
+var Colors;
+(function (Colors) {
+    Colors[Colors["Gray"] = 0] = "Gray";
+    Colors[Colors["Green"] = 1] = "Green";
+    Colors[Colors["Blue"] = 2] = "Blue";
+})(Colors || (Colors = {}));
+var myColor = Colors.Gray;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -1,6 +1,24 @@
 console.log("It works! Hope so..");
-let myName = 'Michal';
-let myAge = 35;
-var hasHobbies = true;
-hasHobbies = "false";
-console.log(hasHobbies);
+let myName: string = 'Michal';
+let myAge: number = 35;
+
+//string array
+let hobbies: string[] = ["silownia", "modele"];
+console.log(typeof(hobbies));
+
+//number array, 
+let numberArray = [100];
+console.log(typeof(numberArray));
+
+//tuple
+let tupleTest:[string, number] = ["tekstowa", 100];
+
+//enum
+enum Colors {
+   Gray,
+   Green,
+   Blue
+}
+
+let myColor: Colors = Colors.Gray;
+console.log(myColor);
